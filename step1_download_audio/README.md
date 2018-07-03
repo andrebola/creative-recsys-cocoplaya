1c952d2d557d2503a576f29726713d51aecd7682
Andres <andresferraro@yahoo.com>
2018-07-03 16:27:19 -0300

    Add script to retrieve url of audio samples

--- step1_download_audio/README.md
+++ step1_download_audio/README.md
@@ -5,4 +5,8 @@ The first step is to download the audio samples for the songs from Spotify, this
 
 `/audio_folder/08/01EADvpwhTIvFgyDAuC108.mp3`
 
-To download the audio a file with the urls is required, to get this file please contact Andres Ferraro (andres.ferraro at upf.edu). After uncompressing the Json file with the urls of the songs, run the python 3 script `download_audio.py`. The folder where the audio will be downloaded can be specified in the script.
+To download the audio samples from Spotify first you need to retrieve the urls of the samples, this is done by running the python 3 script `get_urls.py`. In order to run this script you need to install the same requirement specified in [requiremets file](../step3_recommendations/requirements.txt). This script collects all the songs ids from the MPD, so you need to specify the location of the dataset. 
+
+Also you need to specify the client id and secret of the Spotify API, when you run the script you will be requested to enter a url in the browser and then copy the url which you get redirected and paste it in the console. This is done in order to get an authetication token.
+
+After you retrieved all the urls you need to run the python script `download_audio.py`, which will download the audio. The folder where the audio files will be downloaded can be specified inside the script.
---
+++ step1_download_audio/get_urls.py
@@ -0,0 +1,91 @@
+import tqdm
+import os
+import json
+import spotipy
+import spotipy.util as util
+from collections import defaultdict
+from requests.exceptions import HTTPError
+
+
+def get_all_uris(playlists_path):
+    uris = defaultdict(int)
+    filenames = os.listdir(playlists_path)
+    for filename in sorted(filenames):
+        if filename.startswith("mpd.slice.") and filename.endswith(".json"):
+            fullpath = os.sep.join((playlists_path, filename))
+            f = open(fullpath)
+            js = f.read()
+            f.close()
+            mpd_slice = json.loads(js)
+            for playlist in mpd_slice['playlists']:
+                for track in playlist['tracks']:
+                    uris[track['track_uri'][14:]]
+    return list(uris.keys())
+
+
+def get_urls(uris, client_id, client_secret):
+    partial_output_file = "partial_output.json"
+    username = ""
+    scope = ","
+    redirect_uri = "http://localhost/"
+    token = util.prompt_for_user_token(username, scope,
+            client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    market = "US"
+    existing_uris = {}
+    if os.path.isfile(partial_output_file):
+        existing_uris = json.load(open(partial_output_file, 'r'))
+    all_ids = [x for x in uris if x not in existing_uris]
+    iter_size = 50
+    num_iter = len(all_ids)//iter_size
+    res_iter = len(all_ids)%iter_size
+
+    if token:
+        for _iter in tqdm.tqdm(range(num_iter)):
+            sp = spotipy.Spotify(auth=token)
+            ids = ",".join(all_ids[_iter*iter_size:(_iter+1)*iter_size])
+            try:
+                results = sp._get('tracks?ids=%s&market=%s'%(ids, market), limit=iter_size)
+            except HTTPError:
+                token = util.prompt_for_user_token(username, scope,
+                    client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+            for res in results['tracks']:
+                if res is not None and res['preview_url'] is not None:
+                    url = res['preview_url']
+                    uri = res['uri'][14:]
+                    existing_uris[uri] = url
+            if _iter % (num_iter//10) == 0:
+                json.dump(existing_uris, open(partial_output_file, "w"))
+        if res_iter > 0:
+            sp = spotipy.Spotify(auth=token)
+            ids = ",".join(all_ids[(-1)*res_iter:])
+            results = sp._get('tracks?ids=%s&market=%s'%(ids, market), limit=iter_size)
+            for res in results['tracks']:
+                if res is not None and res['preview_url'] is not None:
+                    url = res['preview_url']
+                    uri = res['uri'][14:]
+                    existing_uris[uri] = url
+        return existing_uris
+    else:
+        print("Error, can't get token for", username)
+        return None
+
+
+if __name__ == '__main__':
+    uris_file = "all_dicts_uri.json"
+    if not os.path.isfile(uris_file):
+        # Specify the location of the MPD:
+        mpd_path = "mpd/data/"
+        all_uris = get_all_uris(mpd_path)
+        json.dump(all_uris, open(uris_file, 'w'))
+    else:
+        all_uris = json.load(open(uris_file, 'r'))
+
+    # Specify the client secret and id of Spotify API:
+    client_secret = ""
+    client_id = ""
+
+    all_dicts = get_urls(all_uris, client_id, client_secret)
+    if all_dicts != None:
+        output_file = "all_dicts.json"
+        json.dump(all_dicts, open(output_file, "w"))
+
